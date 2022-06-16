a989f617d9f444bb759b44395ab45f8cf57b7c1e
UsmanKhanTaimure <107642292+UsmanKhanTaimure@users.noreply.github.com>
2022-06-16 10:42:15 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Chapter_1
+
+// Question_1
+// alert("Error! Please enter a valid password")
+
+// Question_2
+// alert("Welcome To JS Land... \n" +"Happy Coding")
+
+// Question_3
+// alert("Usman Khan")
